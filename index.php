0c27e67f457319c70d8e2c9a26cc238835e56a8c
Willams Silva Andrade <85310845+willamsandrade@users.noreply.github.com>
2021-06-03 15:45:17 -0300

    Add files
    Adicionando Arquivos

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="index.css" />
+</head>
+<body>
+    <section>
+        <div class="container">
+
+            <div class="card">
+                <div class="box">
+                    <div>
+                        <div class="percent">
+                          <svg>
+                             <circle cx="70" cy="70" r="70"></circle>
+                             <circle cx="70" cy="70" r="70"></circle>                                           
+                          </svg>
+                          <div class="number">
+                            <h2>90<span>%</span></h2>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text">
+                    Html 
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="box">
+                    <div>
+                        <div class="percent">
+                          <svg>
+                             <circle cx="70" cy="70" r="70"></circle>
+                             <circle cx="70" cy="70" r="70"></circle>                                           
+                          </svg>
+                          <div class="number">
+                            <h2>82<span>%</span></h2>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text">
+                    CSS
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="box">
+                    <div>
+                        <div class="percent">
+                          <svg>
+                             <circle cx="70" cy="70" r="70"></circle>
+                             <circle cx="70" cy="70" r="70"></circle>                                           
+                          </svg>
+                          <div class="number">
+                            <h2>65<span>%</span></h2>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text">
+                   Javascript 
+                </div>
+            </div>
+
+        </div>
+    </section>
+</body>
+</html>
